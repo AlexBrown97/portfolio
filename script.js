4976bf592eb8d0e057fce291e6bb46c5a514822d
Alex Brown <macuser@Macs-iMac.local>
2020-10-06 18:37:42 +0100

    resize footer for all pages

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ tl.fromTo(".subheading", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".navigation", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".media", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".contact-form", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-
+tl.fromTo(".footer", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".bournemouth", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 const magicalUnderlines = Array.from(
   document.querySelectorAll(".underline--magical")
 );
